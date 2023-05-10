a503ee0e9ff103a10ff092068e7cc70e2a8a432b
Leandro Picoloto <picolotoleandro@gmail.com>
2023-05-10 16:20:52 -0300

    spyOn parte 2

--- src/app/service/spyon-service.service.spec.ts
+++ src/app/service/spyon-service.service.spec.ts
@@ -21,6 +21,8 @@ const RESPONSE = [
 
 describe('SpyonServiceService', () => {
   let service: SpyonServiceService;
+  const logger = jasmine.createSpy('log');
+  const status = jasmine.createSpyObj('service', ['name', 'age', 'email'])
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -39,5 +41,32 @@ describe('SpyonServiceService', () => {
     service.getUsers().subscribe(res => {
       expect(res).toEqual(RESPONSE);
     })
+  });
+
+  it('Deve criar método com jasmine.createSpy', () => {
+    const log = 'loguei no sistema';
+    logger(log);
+
+    expect(logger).toHaveBeenCalledTimes(1);
+    expect(logger).toHaveBeenCalledWith(log);
+  });
+
+  it('Deve criar métodos com jasmine.createSpyObj', () => {
+    const name = 'leandro';
+    const email = 'leandro@leandro.com';
+    const age = 30;
+
+    status.name(name);
+    status.email(email);
+    status.age(age);
+
+    expect(status.name).toHaveBeenCalledTimes(1);
+    expect(status.name).toHaveBeenCalledWith(name);
+
+    expect(status.email).toHaveBeenCalledTimes(1);
+    expect(status.email).toHaveBeenCalledWith(email);
+
+    expect(status.age).toHaveBeenCalledTimes(1);
+    expect(status.age).toHaveBeenCalledWith(age);
   });
 });
