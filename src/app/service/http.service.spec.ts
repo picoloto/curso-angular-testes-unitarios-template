99c50da296d818097e01f58c4daf802f4b01a287
Leandro Picoloto <picolotoleandro@gmail.com>
2023-05-10 17:32:51 -0300

    Testando GET e adição de coverage no launch

--- src/app/service/http.service.spec.ts
+++ src/app/service/http.service.spec.ts
@@ -1,4 +1,7 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { HttpService } from './http.service';
@@ -7,13 +10,15 @@ describe('HttpService', () => {
   let service: HttpService;
   let httpTestingController: HttpTestingController;
   let url: any;
+  let usersUrl: any;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(HttpService);
-    httpTestingController = TestBed.inject(HttpTestingController)
-    url = 'http://localhost:3000'
+    httpTestingController = TestBed.inject(HttpTestingController);
+    url = 'http://localhost:3000';
+    usersUrl = `${url}/users`;
   });
 
   it('should be created', () => {
@@ -22,18 +27,31 @@ describe('HttpService', () => {
 
   it('Deve realizar chamada Get por id', () => {
     const id = 3;
-    const fullUrl = `${url}/users/${id}`;
-    const response = {name: 'Leandro'};
+    const fullUrl = `${usersUrl}/${id}`;
+    const response = { name: 'Leandro' };
 
-    service.getUsersById(id).subscribe(res => {
-      expect(res).toBe(response)
+    service.getUsersById(id).subscribe((res) => {
+      expect(res).toBe(response);
     });
+
     const request = httpTestingController.expectOne(fullUrl);
+    request.flush(response);
 
     expect(request.request.method).toBe('GET');
     expect(request.request.url).toBe(fullUrl);
-
-    request.flush(response);
   });
 
+  it('Deve realizar chamada GET para obter usuários', () => {
+    const response = [{ name: 'Leandro' }, { name: 'Leandro 2' }];
+
+    service.getUsers().subscribe((res) => {
+      expect(res).toBe(response);
+    });
+
+    const request = httpTestingController.expectOne(usersUrl);
+    request.flush(response);
+
+    expect(request.request.method).toBe('GET');
+    expect(request.request.url).toBe(usersUrl);
+  });
 });
