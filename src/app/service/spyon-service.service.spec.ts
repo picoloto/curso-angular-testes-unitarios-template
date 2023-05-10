a003e51303beca2e9bc0a2748456ba0a86ef5fcb
Leandro Picoloto <picolotoleandro@gmail.com>
2023-05-10 16:03:32 -0300

    spyOn

--- src/app/service/spyon-service.service.spec.ts
+++ src/app/service/spyon-service.service.spec.ts
@@ -2,6 +2,22 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { SpyonServiceService } from './spyon-service.service';
+import { of } from 'rxjs/internal/observable/of';
+
+const RESPONSE = [
+  {
+    'name': 'Leandro',
+    'email': 'leandro@teste.com',
+    'age': 30,
+    'id': 1,
+  },
+  {
+    'name': 'Leandro 2',
+    'email': 'leandro 2@teste.com',
+    'age': 31,
+    'id': 2,
+  }
+];
 
 describe('SpyonServiceService', () => {
   let service: SpyonServiceService;
@@ -15,5 +31,13 @@ describe('SpyonServiceService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Deve retornar lista de usuários', () => {
+    spyOn(service, 'getUsers').and.returnValue(of(RESPONSE));
+
+    service.getUsers().subscribe(res => {
+      expect(res).toEqual(RESPONSE);
+    })
   });
 });
