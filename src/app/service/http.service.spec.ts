bf2eb15fc468870d98d2b4ba44a43535dfeb3355
Leandro Picoloto <picolotoleandro@gmail.com>
2023-05-10 17:00:16 -0300

    flush and expectOne

--- src/app/service/http.service.spec.ts
+++ src/app/service/http.service.spec.ts
@@ -5,19 +5,35 @@ import { HttpService } from './http.service';
 
 describe('HttpService', () => {
   let service: HttpService;
-  let htppTestingController: HttpTestingController;
+  let httpTestingController: HttpTestingController;
   let url: any;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule]
     });
     service = TestBed.inject(HttpService);
-    htppTestingController = TestBed.inject(HttpTestingController)
+    httpTestingController = TestBed.inject(HttpTestingController)
     url = 'http://localhost:3000'
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Deve realizar chamada Get por id', () => {
+    const id = 3;
+    const fullUrl = `${url}/users/${id}`;
+    const response = {name: 'Leandro'};
+
+    service.getUsersById(id).subscribe(res => {
+      expect(res).toBe(response)
+    });
+    const request = httpTestingController.expectOne(fullUrl);
+
+    expect(request.request.method).toBe('GET');
+    expect(request.request.url).toBe(fullUrl);
+
+    request.flush(response);
   });
 
 });
