4fc2fccc76f473d3d76f526150b2f29829d7e30c
Leandro Picoloto <picolotoleandro@gmail.com>
2023-05-10 11:54:50 -0300

    toBeTruthy e toBeFalsy

--- src/app/matchers-jasmine/matchers-jasmine.component.spec.ts
+++ src/app/matchers-jasmine/matchers-jasmine.component.spec.ts
@@ -24,14 +24,24 @@ describe('MatchersJasmineComponent', () => {
   it('Deve testar o uso do matcher toEqual', () => {
     expect(true).toEqual(true);
     expect([1,2]).toEqual([1,2])
-    // expect([1,2]).toEqual([1,2,3]) -> O teste considera não apenas length, mas também conteúdo
+    // expect([1,2]).toEqual([1,2,3]) -> O toEqual considera não apenas length, mas também conteúdo
   })
 
   it('Deve testar o uso do matcher toBe', () => {
     let names = ['Leandro', 'Carlos', 'Jose'];
 
     expect(names).toBe(names);
-    // expect(names).toEqual(['Leandro', 'Carlos', 'Jose']) -> O teste considera o mesmo objeto, mesma referencia, etc
+    // expect(names).toEqual(['Leandro', 'Carlos', 'Jose']) -> O toBe considera o mesmo objeto, mesma referencia, etc
+  })
+
+  it('Deve testar o uso do matcher toBeTruthy', () => {
+    expect(true).toBeTruthy();
+    // expect(0).toBeTruthy() -> O toBeTruthy considera a conversão do valor para testar o bool
+  })
+
+  it('Deve testar o uso do matcher toBeFalsy', () => {
+    expect(false).toBeFalsy();
+    // expect(1).toBeTruthy() -> O toBeFalsy considera a conversão do valor para testar o bool
   })
 
 });
