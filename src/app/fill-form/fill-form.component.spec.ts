0027458234e1e360803018f3815598603e22aede
Leandro Picoloto <picolotoleandro@gmail.com>
2023-05-11 11:32:06 -0300

    input.value e input.dispatchEvent

--- src/app/fill-form/fill-form.component.spec.ts
+++ src/app/fill-form/fill-form.component.spec.ts
@@ -10,9 +10,8 @@ describe('FillFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ReactiveFormsModule, FormsModule],
-      declarations: [ FillFormComponent ],
-    })
-    .compileComponents();
+      declarations: [FillFormComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FillFormComponent);
     component = fixture.componentInstance;
@@ -21,5 +20,15 @@ describe('FillFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Deve preencher campos do formulário', () => {
+    let input = fixture.nativeElement.querySelector('input');
+    const value = 'Leandro';
+
+    input.value = value;
+    input.dispatchEvent(new Event('input'));
+
+    expect(input.value).toBe(value);
   });
 });
