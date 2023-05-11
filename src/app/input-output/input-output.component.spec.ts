87e050446a5f0b31834bc3c59cabe6f8dc85555b
Leandro Picoloto <picolotoleandro@gmail.com>
2023-05-11 11:52:40 -0300

    Output e Input de componentes

--- src/app/input-output/input-output.component.spec.ts
+++ src/app/input-output/input-output.component.spec.ts
@@ -8,9 +8,8 @@ describe('InputOutputComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InputOutputComponent ]
-    })
-    .compileComponents();
+      declarations: [InputOutputComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(InputOutputComponent);
     component = fixture.componentInstance;
@@ -21,4 +20,22 @@ describe('InputOutputComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('Deve receber usuário', () => {
+    const user = {
+      name: 'Danilo',
+      email: 'danilodev.silva@gmail.com',
+      password: '091011',
+    };
+    component.user = user;
+
+    expect(component.user).toBe(user);
+  });
+
+  it('Deve emitir mensagem quando clicar no botão', () => {
+    const emitSpy = spyOn(component.userMessage, 'emit');
+    let button = fixture.nativeElement.querySelector('button');
+
+    button.click();
+    expect(emitSpy).toHaveBeenCalled();
+  });
 });
